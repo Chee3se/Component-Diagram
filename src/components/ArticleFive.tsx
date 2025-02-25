bd801f21e780a19b11b6d17a44b021ccb36c3ada
Chee3se <emilspete999@gmail.com>
2025-02-25 23:14:54 +0200

    Rounded last image

--- src/components/ArticleFive.tsx
+++ src/components/ArticleFive.tsx
@@ -8,11 +8,14 @@ export default function ArticleFive() {
                 direction="right"
             >
                 <h2 className="text-6xl font-extrabold mb-6">Kā šī diagramma izskatītos <span className="italic">Blog</span> projektā?</h2>
-                <img
-                    src="/images/DiagramBlog.png"
-                    alt="Diagram Blog"
-                    className="w-full h-[calc(100vh-12rem)] object-contain bg-white rounded-xl shadow-xl"
-                />
+                <div className="relative w-full h-[calc(100vh-12rem)]">
+                    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[42rem] h-[42rem] bg-white rounded-full" />
+                    <img
+                        src="/images/DiagramBlog.png"
+                        alt="Diagram Blog"
+                        className="relative w-full h-full object-contain z-10"
+                    />
+                </div>
             </Reveal>
         </article>
     )
